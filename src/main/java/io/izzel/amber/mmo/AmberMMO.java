9d9708c6fed07bd7579835f9c6822909433c5f21
IzzelAliz <csh2001331@126.com>
2020-01-03 10:36:46 +0800

    Add metrics

--- src/main/java/io/izzel/amber/mmo/AmberMMO.java
+++ src/main/java/io/izzel/amber/mmo/AmberMMO.java
@@ -3,6 +3,7 @@ package io.izzel.amber.mmo;
 import com.google.inject.Inject;
 import io.izzel.amber.commons.i18n.AmberLocale;
 import io.izzel.amber.mmo.drops.DropTableService;
+import io.izzel.amber.mmo.util.Metrics;
 import org.spongepowered.api.plugin.Plugin;
 
 import java.util.Objects;
@@ -11,9 +12,10 @@ import java.util.Objects;
 public class AmberMMO {
 
     @Inject
-    public AmberMMO(AmberLocale locale, DropTableService drop) {
+    public AmberMMO(AmberLocale locale, DropTableService drop, Metrics metrics) {
         Objects.requireNonNull(locale);
         Objects.requireNonNull(drop);
+        Objects.requireNonNull(metrics);
     }
 
 }
